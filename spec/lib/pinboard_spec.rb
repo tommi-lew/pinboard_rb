fce2df1e4a21a542afe7c37710d5ae2020e62151
Tommi Lew <cs.lew.1111@gmail.com>
2012-06-20 20:08:40 +0800

    Added tests.
    Using webmock!

--- spec/lib/pinboard_spec.rb
+++ spec/lib/pinboard_spec.rb
@@ -1,11 +1,22 @@
-require 'spec_helper.rb'
+require 'spec_helper'
 
 describe 'Pinboard' do
+  let(:pb) { Pinboard.new('username', 'password') }
+
+  before (:each) do
+    stub_request(:any, /api.pinboard.in/)
+  end
+
   describe 'API Call'do
-    it 'should call initiate and HTTP GET to Pinboard' do
-      Pinboard.should_receive(:get).and_return("done!")
-      pb = Pinboard.new('username', 'password')
+    it 'should do a HTTP GET to Pinboard without query strings when it has no parameters ' do
       pb.posts.recent.get
+      WebMock.should have_requested(:get, "https://username:password@api.pinboard.in/v1/posts/recent?")
+    end
+
+    it 'should do a HTTP GET to Pinboard with query strings when it has parameters' do
+      pb.posts.recent.params({tag: 'pbrb'}).get
+      WebMock.should have_requested(:get, "https://username:password@api.pinboard.in/v1/posts/recent?").
+        with(:query => {tag: 'pbrb'})
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'pinboard'
+require 'webmock/rspec'
+include WebMock::API
+include WebMock::Matchers
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
