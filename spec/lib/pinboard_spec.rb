459c4ca814ed35ce9f426aefd995fc1aa0f725ef
Tommi Lew <cs.lew.1111@gmail.com>
2012-06-24 11:26:46 +0800

    Refactored specs.

--- spec/lib/pinboard_spec.rb
+++ spec/lib/pinboard_spec.rb
@@ -2,68 +2,54 @@ require 'spec_helper'
 
 describe 'Pinboard' do
   let(:pb) { Pinboard.new('username', 'password') }
+  subject { pb }
 
-  describe 'API Call' do
-    before (:each) do
-      stub_request(:any, /api.pinboard.in/)
+  describe 'API Calls' do
+    context 'without errors' do
+      before { stub_request(:any, /api.pinboard.in/) }
+
+      it 'should do a HTTP GET to Pinboard without query strings when it has no parameters ' do
+        subject.posts.recent.req
+        WebMock.should have_requested(:get, "https://username:password@api.pinboard.in/v1/posts/recent?")
+      end
+
+      it 'should do a HTTP GET to Pinboard with query strings when it has parameters' do
+        subject.posts.recent.params({ tag: 'pbrb' }).req
+        WebMock.should have_requested(:get, "https://username:password@api.pinboard.in/v1/posts/recent?").
+                         with(:query => { tag: 'pbrb' })
+      end
     end
 
-    it 'should do a HTTP GET to Pinboard without query strings when it has no parameters ' do
-      pb.posts.recent.req
-      WebMock.should have_requested(:get, "https://username:password@api.pinboard.in/v1/posts/recent?")
-    end
+    context 'with errors' do
+      before { stub_request(:any, /api.pinboard.in/).to_return(:body => '401 Forbidden') }
 
-    it 'should do a HTTP GET to Pinboard with query strings when it has parameters' do
-      pb.posts.recent.params({tag: 'pbrb'}).req
-      WebMock.should have_requested(:get, "https://username:password@api.pinboard.in/v1/posts/recent?").
-        with(:query => {tag: 'pbrb'})
-    end
-  end
-
-  describe 'API Calls with errors' do
-    before (:each) do
-      stub_request(:any, /api.pinboard.in/).to_return(:body => '401 Forbidden')
-    end
-
-    it 'should raise an InvalidCredentialsError' do
-      lambda { pb.posts.recent.req }.should raise_error Pinboard::InvalidCredentialsError
+      it 'should raise an InvalidCredentialsError' do
+        lambda { subject.posts.recent.req }.should raise_error Pinboard::InvalidCredentialsError
+      end
     end
   end
 
   describe 'implementation of method missing' do
+    let(:calls) { subject.instance_variable_get(:@calls) }
+
     it 'should store name of method calls into an array' do
-      pb.a.b.c
-      calls_array = pb.instance_variable_get(:@calls)
-      calls_array.should be_a_kind_of(Array)
-      calls_array[0].should == :a
-      calls_array[1].should == :b
-      calls_array[2].should == :c
+      subject.a.b.c
+      calls.should be_a_kind_of(Array)
+      calls.should == [:a, :b, :c]
     end
 
     context 'when .params method is invoked' do
       it 'should not store a .params method call into the array' do
-        pb.a.params
-        calls_array = pb.instance_variable_get(:@calls)
-        calls_array.size.should == 1
-        calls_array[0].should == :a
-      end
-
-      it 'should not store a .clear method call into the array' do
-        pb.a.clear
-        calls_array = pb.instance_variable_get(:@calls)
-        calls_array.size.should == 0
+        subject.a
+        expect { subject.params }.to change { calls.size }.by(0)
       end
     end
   end
 
   describe 'able to clear the earlier method calls' do
     it 'should reset array to empty' do
-      pb.a.b
-      calls_array = pb.instance_variable_get(:@calls)
-      calls_array.size.should == 2
-      pb.clear
-      calls_array = pb.instance_variable_get(:@calls)
-      calls_array.should be_empty
+      subject.a.b
+      expect { subject.clear }.to change{ subject.instance_variable_get(:@calls).size}.from(2).to(0)
     end
   end
 
